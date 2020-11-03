59031a14b20587a56e7a67d4b5d7ba16731ff2f5
guyue375 <244017179@qq.com>
2020-11-03 15:08:17 +0800

    base module

---
+++ turboklaus_rocketmq_bus/src/main/java/xyz/turboklaus/turboklaus_rocketmq_bus/TurboklausRocketmqBusApplication.java
@@ -0,0 +1,13 @@
+package xyz.turboklaus.turboklaus_rocketmq_bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboklausRocketmqBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurboklausRocketmqBusApplication.class, args);
+    }
+
+}
---
+++ turboklaus_rocketmq_stream/src/main/java/xyz/turboklaus/turboklaus_rocketmq_stream/TurboklausRocketmqStreamApplication.java
@@ -0,0 +1,13 @@
+package xyz.turboklaus.turboklaus_rocketmq_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboklausRocketmqStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurboklausRocketmqStreamApplication.class, args);
+    }
+
+}
